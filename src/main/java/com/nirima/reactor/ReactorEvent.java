898581c2403c2e14c1d2d10e32dc4203bf35d63f
Nigel Magnay <nigel.magnay@gmail.com>
2015-11-03 09:58:22 +0000

    Whitelist properties of the event.
    Signed-off-by: Nigel Magnay <nigel.magnay@gmail.com>

--- src/main/java/com/nirima/reactor/ReactorEvent.java
+++ src/main/java/com/nirima/reactor/ReactorEvent.java
@@ -2,6 +2,8 @@ package com.nirima.reactor;
 
 import com.google.common.base.Objects;
 
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,11 +12,20 @@ import java.util.Map;
  * Created by magnayn on 02/11/15.
  */
 public class ReactorEvent implements Serializable {
+
+  @Whitelisted
   public String eventName;
 
+  @Whitelisted
   public String jobName;
+
+  @Whitelisted
   public String jobFullName;
+
+  @Whitelisted
   public Integer buildNumber;
+
+  @Whitelisted
   public Map<String, String> eventProperties = new HashMap<>();
 
   @Override
